fc7c756f0fbd0709b300df92886f7cfc007e05d5
LT333 <tuttonlara@gmail.com>
2026-01-14 08:43:47 -0500

    Export default Express app

--- server/index.ts
+++ server/index.ts
@@ -156,8 +156,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
+const port = parseInt(process.env.PORT || "5000", 10);
+httpServer.listen(
     {
       port,
       host: "0.0.0.0",
@@ -168,3 +168,5 @@ app.use((req, res, next) => {
     },
   );
 })();
+
+export default app;
